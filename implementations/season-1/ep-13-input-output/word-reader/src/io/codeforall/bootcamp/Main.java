69fc74978a8d004136da19062230a69b5511767a
Carolina Pereira <carolina.pereira@codeforall.com>
2025-10-16 10:47:54 +0100

    added file copy and word reader implementations

---
+++ implementations/season-1/ep-13-input-output/word-reader/src/io/codeforall/bootcamp/Main.java
@@ -0,0 +1,30 @@
+package io.codeforall.bootcamp;
+
+import java.util.Iterator;
+
+/**
+ * Entry point class to the io.codeforall.bootcamp.WordReader exercise
+ */
+public class Main {
+
+    private static final String FILE_PATH = "resources/lorem_ipsum.txt";
+
+    /**
+     * Entry point
+     * @param args application entry point arguments
+     */
+    public static void main(String[] args) {
+
+        WordReader wordReader = new WordReader(FILE_PATH);
+
+        for (String word: wordReader) {
+            System.out.println(word);
+        }
+
+        Iterator<String> iterator = wordReader.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+    }
+}

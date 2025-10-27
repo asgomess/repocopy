9eb3c7650100e1fc8acb95664572a29a8cc3229d
Carolina Pereira <carolina.pereira@codeforall.com>
2025-10-27 12:31:57 +0000

    added producer consumer implementation

---
+++ implementations/season-2/ep-03-concurrency/producer-consumer/src/io/codeforall/bootcamp/bqueue/Pizza.java
@@ -0,0 +1,22 @@
+package io.codeforall.bootcamp.bqueue;
+
+public class Pizza {
+    private Topping topping;
+
+    public Pizza(){
+        topping = Topping.values()[(int)(Math.random() * Topping.values().length)];
+    }
+
+    @Override
+    public String toString() {
+        return topping.name() + " pizza";
+    }
+
+    private enum Topping {
+        PEPPERONI,
+        PINEAPPLE,
+        MUSHROOM,
+        PEPPERS,
+        EXTRA_CHEESE;
+    }
+}

24bf953f3b9441bcbcece4e542cc89797f5a8531
andreia-pascoa <andreiapascoa@gmail.com>
2025-12-04 15:04:13 +0000

    adds holy-trinity skeleton

---
+++ skeletons/holy-trinity-skeleton/holy-trinity.js
@@ -0,0 +1,7 @@
+const users = require("./users.js");
+
+// - Create a new array with the users that have an odd id;
+// - The new id of the user should be its new index;
+// - Transform the array into an object where the key is the id and the value is the user;
+
+console.log(users);
